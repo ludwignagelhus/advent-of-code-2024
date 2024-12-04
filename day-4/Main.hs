76c9e547398a686d9992b1c532ee8473771eb0e5
Ludwig Nagelhus <ludwignagelhus@gmail.com>
2024-12-04 23:29:59 +0100

    clean stuff up more

--- day-4/Main.hs
+++ day-4/Main.hs
@@ -1,78 +1,58 @@
 module Main where
-
 import Data.List (permutations)
 import Text.Read (readMaybe)
 import Data.Maybe (mapMaybe)
-
 import Util
 
 main :: IO ()
 main = do
-    putStrLn "day 4"
-    -- puzzle <- readInput "input.txt"
-    -- puzzle <- readInput "input-example.txt"
-
-    -- let nRows = length puzzle
-    -- let nCols = length (head puzzle)
-    -- let coords = [(x,y) | x <- [0..nCols-1], y <- [0..nRows-1]]
-
-    -- let total = foldl (\acc coord -> acc + countXmas puzzle coord) 0 coords
-    -- print total
-    -- print $ pt2 puzzle
+    pt1
     pt2
 
+pt1 :: IO ()
+pt1 = do
+    puzzle <- readInput "input.txt"
+    let nRows   = length puzzle; nCols = length (head puzzle)
+        coords  = [(x,y) | x <- [0..nCols-1], y <- [0..nRows-1]]
+        total   = sum . map (countXmas puzzle) $ coords
+    print total
+
 type Puzzle = [String]
-type PuzzleChar = (Coord, Char)
+readInput :: String -> IO Puzzle
+readInput fileName = lines <$> readFile fileName
 
 countXmas :: Puzzle -> Coord -> Int
 countXmas puzzle orig =
-    let east = mapMaybe (getPChar puzzle . addCoords orig) [(0,0), (1,0), (2,0), (3,0)]
-        southEast = mapMaybe (getPChar puzzle . addCoords orig) [(0,0), (1,1), (2,2), (3,3)]
-        south = mapMaybe (getPChar puzzle . addCoords orig) [(0,0), (0,1), (0,2), (0,3)]
-        southWest = mapMaybe (getPChar puzzle . addCoords orig) [(0,0), (-1,1), (-2,2), (-3,3)]
+    let east        = mapMaybe (getPChar puzzle . addCoords orig) [(0,0),  (1,0),  (2,0),  (3,0)]
+        southEast   = mapMaybe (getPChar puzzle . addCoords orig) [(0,0),  (1,1),  (2,2),  (3,3)]
+        south       = mapMaybe (getPChar puzzle . addCoords orig) [(0,0),  (0,1),  (0,2),  (0,3)]
+        southWest   = mapMaybe (getPChar puzzle . addCoords orig) [(0,0), (-1,1), (-2,2), (-3,3)]
     in
-       length $ filter isXmas [east, southEast, south, southWest]
-        
-isXmas :: [Char] -> Bool
-isXmas word = word == "XMAS" || reverse word == "XMAS"
-
--- pt2
+       length $ filter (`elem` ["XMAS", "SAMX"]) [east, southEast, south, southWest]
 
 pt2 :: IO ()
 pt2 = do
     puzzle <- readInput "input.txt"
-    let nRows = length puzzle
-    let nCols = length (head puzzle)
-    let pixels = [(x,y) | x <- [0..nCols-1], y <- [0..nRows-1]]
-    let total = foldl (\acc p -> if isMASAM puzzle p then acc + 1 else acc) 0 pixels
+    let nRows   = length puzzle; nCols = length (head puzzle)
+        coords  = [(x,y) | x <- [0..nCols-1], y <- [0..nRows-1]]
+        total   = length . filter (isMASAM puzzle) $ coords
     print total
-
-readInput :: String -> IO Puzzle
-readInput fileName = 
-    lines <$> readFile fileName
 
 isMASAM :: Puzzle -> Coord -> Bool
 isMASAM puzzle (x,y)
-    | length pChars /= 5            = False
-    | [nw, orig, se] |> masOrSam
-   && [sw, orig, ne] |> masOrSam    = True
-    | otherwise                     = False
+    | length pChars /= 5                                      = False
+    | all (`elem` ["MAS", "SAM"]) [[nw,orig,se],[sw,orig,ne]] = True
+    | otherwise                                               = False
     where
         pChars = getXCoords puzzle (x,y) |> mapMaybe (getPChar puzzle) 
         [nw, ne, orig, sw, se] = pChars
 
 getXCoords :: Puzzle -> Coord -> [Coord]
-getXCoords puzzle orig =
-    [addCoords orig (-1,-1), addCoords orig (1,-1),
-     orig,
-     addCoords orig (-1,1), addCoords orig (1,1)]
+getXCoords puzzle (x,y) = [(x-1, y-1), (x+1,y-1), (x,y), (x-1,y+1), (x+1,y+1)]
 
 getPChar :: Puzzle -> Coord -> Maybe Char
 getPChar puzzle (x,y) =
-    if x < 0 || y < 0 || length puzzle <= y 
-        || length (puzzle !! y) <= x then Nothing
-    else Just (puzzle !! y !! x)
-
-masOrSam :: [Char] -> Bool
-masOrSam word = word == "MAS" || word == "SAM"
+    if x < 0 || y < 0 || y >= length puzzle || x >= length (puzzle !! y)
+        then Nothing
+   else Just (puzzle !! y !! x)
 
