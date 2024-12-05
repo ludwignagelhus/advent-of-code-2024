976a4d114be07aed0a47597d1266e8129bc9bf9f
Ludwig Nagelhus <ludwignagelhus@gmail.com>
2024-12-05 22:12:16 +0100

    checkpt

--- day5/Main.hs
+++ day5/Main.hs
@@ -1,14 +1,81 @@
+import Data.List.Split
 
 main :: IO ()
 main = do
-    exampleInput
+    example
 
-readInput :: String -> IO [Int]
-readInput filename = do
-    content <- readFile filename
-    return $ map read . words . filter (/= '|') content
+type Page = Int
+type PageOrder = [Int]
+type Rule = (Page, Page)
+type RuleMap = [(Page, [Page])]
 
-exampleInput :: IO ()
-exampleInput = do
-    input <- readInput "input-test.txt"
-    print input
+example :: IO ()
+example = do
+    content <- readFile "input-test.txt"
+    let res = parseFileInput content
+    -- print res
+    res2 <- lines <$> readFile "input-test.txt"
+    print res2
+    print $ splitWhen (all (`elem` " \t\n")) res2
+    -- let [ruleLines,updateLines] = splitWhen (== " ") inputLines
+    -- print res
+    -- in case res of
+    -- mapM_ print ruleLines
+   
+    return ()
+
+foo :: IO ()
+foo = do
+    print "foobar"
+    return ()
+
+parseFileInput :: String -> (RuleMap, [PageOrder])
+parseFileInput str
+    | length res < 2        = error "Unexpected input"
+    | otherwise             = ([], [])
+    where
+        xsLines = lines str
+        res = splitWhen (=="\n") xsLines
+
+
+-- getFileInput :: String -> IO (RuleMap, PageOrder)
+-- getFileInput filename
+--     | any length    [ruleLines, updateLines]    = ([],[])
+--     | otherwise = ([], [])
+--     where
+--         content <- lines <$> readFile filename
+--         res@[ruleLines,updateLines] = map (map readInts) $ splitWhen (==" ") content
+--         -- res =  $ splitWhen (==" ") content
+--         print res
+--         return ([], [])
+
+readInts :: String -> [Int]
+readInts = map read . words
+-- mkRules :: [String] -> RuleMap
+-- mkRules = map ((\[a,b] -> (read a, map read b)) . words)
+
+
+-- just need to mk the rule map...
+validPageOrder :: RuleMap -> [Page] -> Bool
+validPageOrder rules pages = False
+-- validPageOrder rules pages
+
+-- passesRule :: -> Rule -> [Page] -> Bool
+-- passesRule _ [] = True
+-- passesRule (a,b) ps = False
+
+middlePage :: [Page] -> Page
+middlePage [x] = x
+middlePage [x,y] = (x * y) `div` 2
+middlePage (x:xs) = middlePage $ dropLast xs
+
+dropLast :: [a] -> [a]
+dropLast [x] = []
+dropLast (x:xs) = x : dropLast xs
+
+getRules :: RuleMap -> Page -> [Page]
+getRules rm p =
+    let rules = lookup p rm
+    in case rules of
+        Just ps -> ps
+        Nothing -> []
---
+++ erf.md
@@ -0,0 +1,23 @@
+# ghci -> load a module:
+
+```
+ghci> :load Main
+ghci> foobar
+```
+
+# ghcid -> run repl reloading on file changes:
+
+```
+~/.local/bin/ghcid --c "ghci Main.hs" --test "main"
+```
+
+# haskell tools:
+
+- ghc
+- ghci
+- ghcid
+- hlint
+- ormolu
+- hls
+- cabal
+- stack
