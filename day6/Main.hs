8dc0fd6b6c04749ce400ab6239dd47a0fb9b116a
Ludwig Nagelhus <ludwignagelhus@gmail.com>
2024-12-06 20:48:20 +0100

    solve pt 1

--- day6/Main.hs
+++ day6/Main.hs
@@ -13,79 +13,73 @@ main = do
     let map = parseInput content
     mapM_ (putStrLn . show) map
 
--- multiple monadic contexts? monad stacks?
+pt1 :: IO ()
+pt1 = do
+    area <- parseInput <$> readFile "input.txt"
+    let guard = findGuard area
+    case guard of
+        Nothing -> error "No guard found."
+        Just (origin, dir) -> print $ length $ nub $ patrol area [origin] dir
+
+pt2 :: IO ()
+pt2 = do
+    area <- parseInput <$> readFile "input.txt"
+    let guard = findGuard area
+    return ()
+
 expfn :: IO ()
 expfn = do
-    -- map <- parseInput <$> readFile "input-test.txt"
-    map <- parseInput <$> readFile "input.txt"
-    let guard = findGuard map
-    case guard of
-        Nothing -> error "No guard found"
-        Just g -> do
-            let path = walk map g
-            mapM_ putStrLn map
-            print path
-            print (length $ nub path)
+    -- area <- parseInput <$> readFile "input-test.txt"
+    pt1
+    -- pt2
 
-    -- let path = walk map guard
-    -- print path
-
-type Coordinate = (Int, Int)
 type AreaMap = [[Char]]
-data Direction = North | South | West | East deriving (Eq, Show)
-type Guard = (Coordinate, Direction)
 
 parseInput :: String -> AreaMap
-parseInput str =
-    let xsLines = (lines str) :: [[Char]]
-        result = xsLines
-    in result
+parseInput str = (lines str) :: AreaMap
 
-findGuard :: AreaMap -> Maybe Guard
-findGuard rs = do
-    row <- findIndex (elem '^') rs
-    col <- elemIndex '^' (rs !! row)
-    return ((row, col), North)
+type Coordinate = (Int, Int)
+data Direction = North | South | West | East deriving (Eq, Show)
 
--- state monad possible here?
+findGuard :: AreaMap -> Maybe (Coordinate, Direction)
+findGuard area = do
+    y <- findIndex (elem '^') area
+    x <- elemIndex '^' (area !! y)
+    return ((y, x), North)
+
 type Path = [Coordinate]
-walk :: AreaMap -> Guard -> Path
-walk area (posPlayer, dir) = walk' area (posPlayer, dir) [posPlayer]
 
-walk' :: AreaMap -> Guard -> Path -> Path
-walk' area guard@(posPlayer, dir) path
-    | not (visible area posNext)    = path
-    | square area posNext == '#'    = walk' area (turnRight guard) path
-    | otherwise                     = walk' area (posNext, dir) (posNext : path)
-        where
-            posNext = moveOne guard
+patrol :: AreaMap -> Path -> Direction -> Path
+patrol area path dir
+    | null path                     = error "No starting point."
+    | not (visible area nextPos)    = path
+    | nextSquare == '#'             = patrol area path (turnRight dir) 
+    | otherwise                     = patrol area (nextPos : path) dir
+    where
+        nextPos = moveOne (head path) dir
+        nextSquare = square area nextPos
 
-turnRight :: Guard -> Guard
-turnRight (pos, dir)
-    | dir == North = (pos, East)
-    | dir == East = (pos, South)
-    | dir == South = (pos, West)
-    | dir == West = (pos, North)
-
-moveOne :: Guard -> Coordinate
-moveOne (pos, dir) = case dir of
-    North -> up pos
-    East -> right pos
-    South -> down pos
-    West -> left pos
-
--- nicer way to write this?
 visible :: AreaMap -> Coordinate -> Bool
 visible area (x, y)
-    | any (<0) [x,y]             = False
+    | any (<0) [x,y]            = False
     | x >= length (head area)   = False
     | y >= length area          = False
     | otherwise                 = True
 
-square :: AreaMap -> Coordinate -> Char
-square area (r, c) = (area !! r) !! c
+turnRight :: Direction -> Direction
+turnRight dir = case dir of
+    North -> East
+    East -> South
+    South -> West
+    West -> North
 
-up (r, c) = (r - 1, c)
-down (r, c) = (r + 1, c)
-left (r, c) = (r, c - 1)
-right (r, c) = (r, c + 1)
+moveOne :: Coordinate -> Direction -> Coordinate
+moveOne (x, y) dir = case dir of
+    North -> (x, y - 1)
+    East -> (x + 1, y)
+    South -> (x, y + 1)
+    West -> (x - 1, y)
+
+square :: AreaMap -> Coordinate -> Char
+square area (x, y) = (area !! y) !! x
+
