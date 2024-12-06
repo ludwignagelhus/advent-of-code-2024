f8c07fec3e2baa34496d9673a47c8ac02b585d80
Ludwig Nagelhus <ludwignagelhus@gmail.com>
2024-12-06 03:42:37 +0100

    solve pt 1

--- day5/Main.hs
+++ day5/Main.hs
@@ -1,42 +1,48 @@
 module Main where
-import Data.List.Split
-import Data.List (groupBy)
-import Data.Map (Map, fromListWith)
-import Control.Arrow ((>>>))
+import qualified Data.Map as Map
+import Data.Map (Map, fromListWith, toList)
+import Data.List (intersect, splitAt)
+import Data.Maybe (fromMaybe)
+-- import Control.Lens ((&))
 
 main :: IO ()
 main = do
     example
 
 type Page = Int
-type PageOrder = [Int]
-type Updates = [Page]
+type Update = [Page]
 type Rule = (Page, Page)
 type RuleMap = Map Page [Page]
 
--- read about operator... specificity?
 (|>) :: a -> (a -> b) -> b
 x |> f = f x
 
 example :: IO ()
 example = do
-    content <- readFile "input-test.txt"
-    let intLists         = content |> (replace ['|', ','] ' ') |> lines |> map lineToInts
-        rules            = intLists |> takeWhile (not . null) |> map (\[a,b] -> (a, b))
-        updates          = intLists |> (drop 1 . dropWhile (not . null))
+    content <- readFile "input.txt"
+    let (rules, updates) = parseFileInput content
 
-    mapM_ print rules
-    mapM_ print updates
+    let ruleMap = mkRuleMap rules
+    print ruleMap
+    mapM_ print (toList ruleMap)
 
-    -- let ruleMap = mkRuleMap rules
+    let valids = filter (okUpdate ruleMap) updates
+    print valids
+    print $ sum $ map (middlePage) valids
     -- print ruleMap
 
-    -- case res of
-    --     [rules, updates] -> print rules
-    --     _ -> error "Unexpected input"
+    return ()
+
+parseFileInput :: String -> ([Rule], [Update])
+parseFileInput content =
+    let intLists         = content |> (replace ['|', ','] ' ') |> lines |> map lineToInts
+        rules            = intLists |> takeWhile (not . null) |> map (\[a,b] -> (a, b))
+        updates          = intLists |> dropWhile (not . null) |> drop 1
+    in (rules, updates)
 
 lineToInts :: String -> [Int]
 lineToInts = map (read :: String -> Int) . words
+-- lineToInts = words |> map (read :: String -> Int)
 
 replace :: Eq a => [a] -> a -> [a] -> [a]
 replace targets with = map (\x -> if x `elem` targets then with else x)
@@ -44,15 +50,18 @@ replace targets with = map (\x -> if x `elem` targets then with else x)
 mkRuleMap :: [Rule] -> RuleMap
 mkRuleMap = fromListWith (++) . map (\(a,b) -> (a, [b]))
 
--- just need to mk the rule map...
-validUpdates :: RuleMap -> Updates -> Bool
-validUpdates _ [] = True
-validUpdates rules (x:xs) = False
--- validPageOrder rules pages
+okUpdate :: RuleMap -> Update -> Bool
+okUpdate rm ps = okUpdate' rm (reverse ps)
+    where
+        okUpdate' _ [] = True
+        okUpdate' _ [_] = True
+        okUpdate' rm (p:ps) =
+            if not . null $ ps `intersect` (getRules rm p)
+              then False
+              else okUpdate' rm ps
 
--- passesRule :: -> Rule -> [Page] -> Bool
--- passesRule _ [] = True
--- passesRule (a,b) ps = False
+getRules :: RuleMap -> Page -> [Page]
+getRules rm p = fromMaybe [] (Map.lookup p rm)
 
 middlePage :: [Page] -> Page
 middlePage [x] = x
@@ -62,10 +71,3 @@ middlePage (x:xs) = middlePage $ dropLast xs
 dropLast :: [a] -> [a]
 dropLast [x] = []
 dropLast (x:xs) = x : dropLast xs
-
--- getRules :: RuleMap -> Page -> [Page]
--- getRules rm p =
---     let rules = lookup p rm
---     in case rules of
---         Just ps -> ps
---         Nothing -> []
